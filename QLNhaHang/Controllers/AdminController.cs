e8acd19475fc9c92130f1c202d3730c9011501cf
Linh4767 <ngoclinh24032004@gmail.com>
2024-12-30 23:13:48 +0700

    QLMonAn

--- QLNhaHang/Controllers/AdminController.cs
+++ QLNhaHang/Controllers/AdminController.cs
@@ -29,5 +29,130 @@ namespace QLNhaHang.Controllers
         {
             return View();
         }
+        /*
+         * Quản lý loại món ăn
+         */
+        //Danh sách loại món ăn
+        public IActionResult DanhSachLoaiMonAn_Admin()
+        {
+            var dsLoaiMA = _QLNhaHangContext.LoaiMonAns.ToList();
+            return View(dsLoaiMA);
+        }
+        //Thêm loại món ăn
+        //Tạo mã tự động
+        public string TaoMaLoaiMATuDong()
+        {
+            //Lấy danh sách loại món ăn
+            var dsLoaiMA = _QLNhaHangContext.LoaiMonAns.ToList();
+            //Tìm mã loại món ăn lớn
+            int maLoaiMALonNhat = dsLoaiMA
+                                 .Select(loaiMA => int.Parse(loaiMA.MaLoaiMa.Substring(3)))
+                                 .Max();  // Lấy số lớn nhất
+            //Tăng chức vụ lớn nhất lên 1
+            int maLoaiMAHT = maLoaiMALonNhat + 1;
+            return "LMA" + maLoaiMAHT.ToString("D3");
+        }
+        //Thêm loại món ăn
+        public IActionResult ThemLoaiMA()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ThemLoaiMA(LoaiMonAn loaiMA)
+        {
+            if (!string.IsNullOrEmpty(loaiMA.TenLoaiMa))
+            {
+                if (!_QLNhaHangContext.LoaiMonAns.Any(loaiMonAn => loaiMonAn.TenLoaiMa == loaiMA.TenLoaiMa))
+                {
+                    loaiMA.MaLoaiMa = TaoMaLoaiMATuDong();
+                    _QLNhaHangContext.LoaiMonAns.Add(loaiMA);
+                    _QLNhaHangContext.SaveChanges();
+                    TempData["ThongBaoThemTC"] = "Thêm danh mục món ăn thành công";
+                    return RedirectToAction("DanhSachLoaiMonAn_Admin");
+                }
+                else
+                {
+                    TempData["ThongBaoThemLoi"] = "Danh mục món ăn đã tồn tại";
+                    return RedirectToAction("ThemLoaiMA");
+                }
+            }
+            else
+            {
+                TempData["ThongBaoTrong"] = "Vui lòng không để trống tên loại món ăn";
+                return RedirectToAction("ThemLoaiMA");
+            }
+        }
+        //Xóa loại món ăn
+        public IActionResult XoaLoaiMA(string maLoaiMA)
+        {
+            // Kiểm tra nếu có món ăn đang bán
+            var dsMonAn = _QLNhaHangContext.MonAns.Where(ma => ma.LoaiMa == maLoaiMA && ma.TrangThai == "Đang bán").ToList();
+            if (dsMonAn.Any())
+            {
+                TempData["ThongBaoXoaLoi"] = "Danh sách món ăn trong mục vẫn đang được bán. Danh mục không thể xóa.";
+                return RedirectToAction("DanhSachLoaiMonAn_Admin");
+            }
+
+            // Nếu không có món ăn đang bán, tiếp tục xử lý các món ăn ngừng bán
+            var dsMonAnNgungBan = _QLNhaHangContext.MonAns.Where(ma => ma.LoaiMa == maLoaiMA && ma.TrangThai == "Ngừng bán").ToList();
+            if (dsMonAnNgungBan.Any())
+            {
+                foreach (var i in dsMonAnNgungBan)
+                {
+                    i.LoaiMa = null; // Cập nhật lại LoaiMa của món ăn đang ngừng bán
+                }
+                _QLNhaHangContext.SaveChanges(); // Lưu thay đổi vào cơ sở dữ liệu
+            }
+
+            // Xóa danh mục món ăn
+            var danhMuc = _QLNhaHangContext.LoaiMonAns.FirstOrDefault(s => s.MaLoaiMa == maLoaiMA);
+            if (danhMuc != null)
+            {
+                _QLNhaHangContext.LoaiMonAns.Remove(danhMuc);
+                _QLNhaHangContext.SaveChanges(); // Lưu thay đổi vào cơ sở dữ liệu
+                TempData["ThongBaoXoaTC"] = "Xóa danh mục món ăn thành công."; // Thông báo thành công
+            }
+
+            return RedirectToAction("DanhSachLoaiMonAn_Admin");
+        }
+        //Sửa loại món ăn
+        public IActionResult SuaLoaiMA(string maLoaiMA)
+        {
+            var loaiMA = _QLNhaHangContext.LoaiMonAns.Where(lma => lma.MaLoaiMa == maLoaiMA).FirstOrDefault();
+            return View(loaiMA);
+        }
+        [HttpPost]
+        public IActionResult SuaLoaiMA(LoaiMonAn loaiMA)
+        {
+            if (!string.IsNullOrEmpty(loaiMA.TenLoaiMa))
+            {
+                if (!_QLNhaHangContext.LoaiMonAns.Any(loaiMonAn => loaiMonAn.MaLoaiMa != loaiMA.MaLoaiMa &&loaiMonAn.TenLoaiMa == loaiMA.TenLoaiMa))
+                {
+                    _QLNhaHangContext.LoaiMonAns.Update(loaiMA);
+                    _QLNhaHangContext.SaveChanges();
+                    TempData["ThongBaoSuaTC"] = "Cập nhật danh mục món ăn thành công";
+                    return RedirectToAction("DanhSachLoaiMonAn_Admin");
+                }
+                else
+                {
+                    TempData["ThongBaoSuaLoi"] = "Danh mục món ăn đã tồn tại";
+                    return RedirectToAction("SuaLoaiMA");
+                }
+            }
+            else
+            {
+                TempData["ThongBaoTrong"] = "Vui lòng không để trống tên loại món ăn";
+                return RedirectToAction("SuaLoaiMA");
+            }
+        }
+        //Tìm kiếm tên danh mục
+        [HttpGet]
+        public IActionResult TimKiemMonAn(string tuKhoa)
+        {
+            var dsTimKiem = string.IsNullOrEmpty(tuKhoa) ? _QLNhaHangContext.LoaiMonAns.ToList() : _QLNhaHangContext.LoaiMonAns.Where(lma => lma.TenLoaiMa.Contains(tuKhoa)).ToList();
+            return PartialView("_LoaiMATableContainer", dsTimKiem);
+
+        }
     }
 }
+
