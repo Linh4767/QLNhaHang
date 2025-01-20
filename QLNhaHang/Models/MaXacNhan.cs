273f7009c60d68db65821a1a591fc016001d4431
ZinhZinh <22211TT2017@mail.tdc.edu.vn>
2025-01-20 20:52:43 +0700

    aa

---
+++ QLNhaHang/Models/MaXacNhan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QLNhaHang.Models
+{
+    public partial class MaXacNhan
+    {
+        public int Id { get; set; }
+        public string MaXacNhan1 { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public DateTime? NgayDatBan { get; set; }
+        public string? TenKh { get; set; }
+        public string? TrangThai { get; set; }
+        public int? SoNguoiDi { get; set; }
+        public string? Sdt { get; set; }
+        public DateTime? NgayTao { get; set; }
+    }
+}
